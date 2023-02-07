619ce6fa1f4d13666711cc7754c9704e34c3df39
RCurvo <renan.curvo@gmail.com>
2023-02-07 15:37:07 -0300

    feat: add Input Component

--- src/screens/Register/styles.ts
+++ src/screens/Register/styles.ts
@@ -3,6 +3,7 @@ import { RFValue } from 'react-native-responsive-fontsize'
 
 export const Container = styled.View`
   flex: 1;
+  background-color: ${({ theme }) => theme.colors.background};
 `
 
 export const Header = styled.View`
@@ -18,4 +19,9 @@ export const Title = styled.Text`
   font-family: ${({ theme }) => theme.fonts.regular};
   font-size: ${RFValue(18)}px;
   color: ${({ theme }) => theme.colors.shape};
+`
+export const Form = styled.View`
+  flex: 1;
+  width: 100%;
+  padding: 24px;
 `
