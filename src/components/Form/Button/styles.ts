2968108171803d17564a0c293282699025caef9f
RCurvo <renan.curvo@gmail.com>
2023-02-07 16:51:39 -0300

    feat: add Button Component

--- src/components/Form/Button/styles.ts
+++ src/components/Form/Button/styles.ts
@@ -1,5 +1,17 @@
+import { TouchableOpacity } from 'react-native'
+import { RFValue } from 'react-native-responsive-fontsize'
 import styled from 'styled-components/native'
 
-export const Container = styled.View`
-  flex: 1;
+export const Container = styled(TouchableOpacity)`
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.secondary};
+  align-items: center;
+  justify-content: center;
+  border-radius: 5px;
+`
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.medium};
+  font-size: ${RFValue(14)}px;
+  color: ${({ theme }) => theme.colors.shape};
+  padding: 18px;
 `
