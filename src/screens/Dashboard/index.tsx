f444f5ab4b3ced2426c75c0b633e798df6d48ef3
RCurvo <renan.curvo@gmail.com>
2023-02-12 12:01:26 -0300

    lint: remove comented code

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -144,32 +144,6 @@ export function Dashboard() {
     }, []),
   )
 
-  // const data: DataListProps[] = [
-  //   {
-  //     id: '1',
-  //     type: 'positive',
-  //     title: 'Desenvolvimento de site',
-  //     amount: 'R$ 12.000,00',
-  //     category: { name: 'Vendas', icon: 'dollar-sign' },
-  //     date: '13/04/2022',
-  //   },
-  //   {
-  //     id: '2',
-  //     type: 'negative',
-  //     title: 'Hamburguer',
-  //     amount: 'R$59,00',
-  //     category: { name: 'Alimentação', icon: 'coffee' },
-  //     date: '10/04/2022',
-  //   },
-  //   {
-  //     id: '3',
-  //     type: 'negative',
-  //     title: 'Aluguel',
-  //     amount: 'R$ 1.200,00',
-  //     category: { name: 'Casa', icon: 'shopping-bag' },
-  //     date: '10/04/2022',
-  //   },
-  // ]
   return (
     <Container>
       {isLoading ? (
